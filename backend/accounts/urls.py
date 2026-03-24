9cfaaee383a8ad69794ca15e1a58afac6500d9ae
dhruvsakthi <dhruv.sakthivel@gmail.com>
2026-03-24 15:10:18 -0400

    removed frontend tests, cleaned up models/api to use checkout after bookings completed

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -29,7 +29,7 @@ urlpatterns = [
     path("bookings/current/", views.my_current_bookings),
     path("bookings/past/", views.my_past_bookings),
     path("payments/history/", views.my_payment_history),
-    path("payments/test-checkout/", views.create_test_checkout_session),
+    path("payments/checkout/deposit/", views.create_deposit_checkout_session),
     path("payments/webhook/", views.stripe_webhook),
     path("favorites/", views.my_favorite_listings),
     path("favorites/<int:listing_id>/", views.favorite_listing_toggle),
