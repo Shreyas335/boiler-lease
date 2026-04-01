c6e19f08c41a17fbc842a6225dd8f22dee584d3c
dhruvsakthi <dhruv.sakthivel@gmail.com>
2026-04-01 15:59:48 -0400

    migrations error fix

--- backend/accounts/migrations/0013_merge_20260324_2300.py
+++ backend/accounts/migrations/0013_merge_20260324_2300.py
@@ -5,7 +5,7 @@ class Migration(migrations.Migration):
     dependencies = [
         ("accounts", "0010_merge_20260326_0435"),
         ("accounts", "0011_propertybooking_status"),
-        ("accounts", "0012_transaction_record"),
+        ("accounts", "0012_user_identity_verification"),
     ]
 
     operations = []
